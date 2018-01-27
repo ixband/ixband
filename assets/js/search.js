b16b91d76dee5c663c7a715ddf6620099db6e019
blaxk <dev@blaxk.com>
2018-01-27 20:02:36 +0900

    docs 수정

--- assets/js/search.js
+++ assets/js/search.js
@@ -21,23 +21,21 @@
     }
 
     function drawList ( keyword, docs ) {
-        var $result = $( '.result > ul' ),
-            length = docs.length,
+        var length = docs.length,
             reg = new RegExp( keyword, 'i' ),
             html = '';
 
         for ( var i = 0; i < length; ++i ) {
-            var doc = docs[i];
+            var doc = docs[i],
+                desc = decodeURIComponent( doc.description.join('<br>') );
 
-            if ( reg.test(doc.title) || _.contains(doc.tags, keyword) || match(reg, doc.description) ) {
+            if ( reg.test(doc.title) || _.contains(doc.tags, keyword) || reg.test(desc) ) {
                 var item = [
                     '<li class="item">',
                         '<a href="' + doc.path + '">',
                             '<h3>' + doc.title + '</h3>',
                         '</a>',
-                        '<div class="desc">',
-                            decodeURIComponent( doc.description.join('<br>') ),
-                        '</div>',
+                        '<div class="desc">' + desc + '</div>',
                     '</li>'
                 ];
 
@@ -45,25 +43,15 @@
             }
         }
 
-        $result.html( html );
+        if ( html ) {
+            $( '.result > ul' ).html( html );
+        } else {
+            drawNoResult ( keyword );
+        }
     }
 
     function drawNoResult ( keyword ) {
         $( '.result' ).html( '<span class="keyword">"' + keyword  + '"</span> 의 대한 검색 결과가 없습니다.' );
-    }
-
-    function match ( reg, ary ) {
-        var result = false,
-            length = ary.length;
-
-        for ( var i = 0; i < length; ++i ) {
-            if ( reg.test(ary[i]) ) {
-                result = true;
-                break;
-            }
-        }
-
-        return result;
     }
 
 })( jQuery, ixBand );
