4d4af40a3cad5953d1bf8973180517bf9e068be6
blaxk <dev@blaxk.com>
2018-01-27 20:18:50 +0900

    docs 수정

--- assets/js/search.js
+++ assets/js/search.js
@@ -26,16 +26,17 @@
             html = '';
 
         for ( var i = 0; i < length; ++i ) {
-            var doc = docs[i],
-                desc = decodeURIComponent( doc.description.join('<br>') );
+            var doc = docs[i];
 
-            if ( reg.test(doc.title) || _.contains(doc.tags, keyword) || reg.test(desc) ) {
+            decode( doc.description );
+
+            if ( reg.test(doc.title) || _.contains(doc.tags, keyword) || match( reg, doc.description) ) {
                 var item = [
                     '<li class="item">',
                         '<a href="' + doc.path + '">',
                             '<h3>' + doc.title + '</h3>',
                         '</a>',
-                        '<div class="desc">' + desc + '</div>',
+                        '<div class="desc">' + doc.description.splice(0, 2).join('<br>') + '</div>',
                     '</li>'
                 ];
 
@@ -52,6 +53,28 @@
 
     function drawNoResult ( keyword ) {
         $( '.result' ).html( '<span class="keyword">"' + keyword  + '"</span> 의 대한 검색 결과가 없습니다.' );
+    }
+
+    function decode ( ary ) {
+        var length = ary.length;
+
+        for ( var i = 0; i < length; ++i ) {
+            ary[i] = decodeURIComponent( ary[i] );
+        }
+    }
+
+    function match ( reg, ary ) {
+        var result = false,
+            length = ary.length;
+
+        for ( var i = 0; i < length; ++i ) {
+            if ( reg.test(ary[i]) ) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
     }
 
 })( jQuery, ixBand );
