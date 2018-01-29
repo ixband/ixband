28b7becbd4dbdc110f4785f8b7dbaac337528dcb
blaxk <dev@blaxk.com>
2018-01-29 22:48:03 +0900

    docs event 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,6 +4,9 @@
 ;(function ( $, $B ) {
     'use strict';
 
-
+    //Category
+    $( '.categories .category-name' ).on( 'click', function () {
+        $( this ).parent().toggleClass( 'active' );
+    });
 
 })( jQuery, ixBand );
